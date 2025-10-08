10d6f64087ef40bd81f47f62c193b0b9b458ac7c
Stu Finn <stu@borealisweb.ca>
2025-10-08 11:12:45 -0400

    Create list of dictionaries and sort them

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import count_words, count_characters
+from stats import count_words, count_characters, create_list_of_dict, sort_list_of_dicts
 
 def get_book_text(file_path):
     file_contents = ""
@@ -12,6 +12,8 @@ def main():
     print(f"Found {word_count} total words")
     # print(book_text)  # Print the first 1000 characters of the book
     char_count_dictionary = count_characters(book_text)
-    print(char_count_dictionary) 
+    list_of_cc_dict = create_list_of_dict(char_count_dictionary)
+    sorted_list = sort_list_of_dicts(list_of_cc_dict)
+    print(sorted_list)
 
 main()
--- stats.py
+++ stats.py
@@ -12,3 +12,31 @@ def count_characters(string):
         else:
             character_count[lower_char] = character_count[lower_char] + 1
     return character_count
+
+def create_list_of_dict(dict):
+    new_list=[]
+     # Iterate over the items in each dictionary   
+    for key in dict:
+        new_list.append({"char": key, "num": dict[key] })
+    return new_list
+
+def sort_on(items):
+    return items["num"]
+
+# Given a list of dictionaries, sort them
+def sort_list_of_dicts(list_of_dicts):
+    # print(list_of_dicts)
+    list_of_dicts.sort(reverse=True, key=sort_on)
+
+    return list_of_dicts
+
+
+
+
+
+
+# def sort_on(items):
+#     return items[""]
+
+# 1. Create a list of dictionaries - one for each character
+# e.g. [ { char: a, num: 456 }, { char: b, num: 34 } ]
