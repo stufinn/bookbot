e02cdc2a9ed4dfc09383e816927a673c754a2098
Stu Finn <stu@borealisweb.ca>
2025-10-08 11:23:34 -0400

    Print output from bookbot

--- main.py
+++ main.py
@@ -9,11 +9,19 @@ def get_book_text(file_path):
 def main():
     book_text = get_book_text("books/frankenstein.txt")
     word_count = count_words(book_text)
+    
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
     print(f"Found {word_count} total words")
+    print("--------- Character Count -------")
     # print(book_text)  # Print the first 1000 characters of the book
     char_count_dictionary = count_characters(book_text)
     list_of_cc_dict = create_list_of_dict(char_count_dictionary)
     sorted_list = sort_list_of_dicts(list_of_cc_dict)
-    print(sorted_list)
+    # print(sorted_list)
+    for item in sorted_list:
+        print(f"{item["char"]}: {item["num"]}")
+    print("============= END ===============")
 
 main()
